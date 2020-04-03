ce54634f889cd5e8510ad6d1bd7760a834e349f5
axmonsalve <axel.monsalve.ponce@gmail.com>
2020-04-03 20:56:17 -0300

    he vuelto atrás

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const UIBtnVerMas = document.querySelectorAll('.boton-ver-mas');
+const UIBtnVerMasCofres = document.querySelectorAll('.ver-mas-cofres');
 
 UIBtnVerMas.forEach((button) => {
   button.addEventListener('click', function(e){
@@ -11,6 +12,22 @@ UIBtnVerMas.forEach((button) => {
       button.textContent = 'Ver mas'
       button.classList.add('boton-ver-mas')
       button.classList.remove('boton-ver-menos')
+    }
+
+  });
+});
+
+UIBtnVerMasCofres.forEach((button) => {
+  button.addEventListener('click', function(e){
+    e.preventDefault();
+    if(!e.target.classList.contains('ver-mas-cofres')){
+      button.textContent = 'Ocultar cofres'
+      button.classList.remove('ver-mas-cofres')
+      button.classList.add('ver-menos-cofres')
+    }else{
+      button.textContent = 'Ver cofres'
+      button.classList.add('ver-mas-cofres')
+      button.classList.remove('ver-menos-cofres')
     }
 
   });
